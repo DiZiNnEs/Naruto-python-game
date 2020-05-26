a397d27b389335b12ffdb62432601342be3788dc
dizinnes <dizinnes228@gmail.com>
2020-05-26 17:16:47 +0600

    naruto-game: performed moving for four players

--- game_modes/game_for_four_players.py
+++ game_modes/game_for_four_players.py
@@ -7,6 +7,8 @@ from ninja_settings import second_player
 from ninja_settings import third_player
 from ninja_settings import fourth_player
 
+from .move_settings import move_for_four_players as move
+
 
 def start_game():
     pygame.init()
@@ -25,10 +27,15 @@ def start_game():
 
         window.blit(ninja('back/background.jpg'), (0, 0))
 
-        window.blit(ninja('ninja/sasori.png'), (first_player(100, 750)))
-        window.blit(ninja('ninja/sasori.png'), (second_player(500, 700)))
-        window.blit(ninja('ninja/sasori.png'), (third_player(1200, 750)))
-        window.blit(ninja('ninja/sasori.png'), (fourth_player(1700, 700)))
+        # window.blit(ninja('ninja/sasori.png'), (first_player(100, 750)))
+        # window.blit(ninja('ninja/sasori.png'), (second_player(500, 700)))
+        # window.blit(ninja('ninja/sasori.png'), (third_player(1200, 750)))
+        # window.blit(ninja('ninja/sasori.png'), (fourth_player(1700, 700)))
+
+        window.blit(ninja('ninja/itachi.png'), (move.first_player(100, 650)))
+        window.blit(ninja('ninja/itachi.png'), (move.second_player(500, 650)))
+        window.blit(ninja('ninja/itachi.png'), (move.third_player(1200, 650)))
+        window.blit(ninja('ninja/itachi.png'), (move.fourth_player(1700, 650)))
 
         pygame.display.update()
 
